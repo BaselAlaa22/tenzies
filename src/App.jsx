cee636b5b9e5852b50e1ec79431843be9b12d3d6
BaselAlaa22 <102911455+BaselAlaa22@users.noreply.github.com>
2025-01-21 19:05:41 +0200

    Complete project on both scriba and my package

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import Die from "./Die.jsx";
 import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 export default function App() {
-  const [dice, setDice] = useState(generateAllNewDice());
+  const [dice, setDice] = useState(() => generateAllNewDice());
+  const newGameBtn = useRef(null);
+
+  const gameWon = dice.every((die) => {
+    return die.value === dice[0].value && die.isHeld;
+  });
+
+  useEffect(() => {
+    if (gameWon) {
+      newGameBtn.current.focus();
+    }
+  }, [gameWon]);
 
   function generateAllNewDice() {
     const arr = [];
@@ -11,25 +23,58 @@ export default function App() {
     for (let i = 0; i < 10; i++) {
       arr[i] = {
         value: Math.floor(Math.random() * 6) + 1,
-        isHeld: true,
+        isHeld: false,
         id: nanoid(),
       };
     }
     return arr;
   }
 
+  function hold(id) {
+    setDice((prevDice) =>
+      prevDice.map((die) =>
+        die.id === id ? { ...die, isHeld: !die.isHeld } : die
+      )
+    );
+  }
+
   function rollDice() {
-    setDice(generateAllNewDice());
+    if (gameWon) {
+      setDice(generateAllNewDice);
+    } else {
+      setDice((prevDice) =>
+        prevDice.map((die) =>
+          die.isHeld
+            ? die
+            : { ...die, value: Math.floor(Math.random() * 6) + 1 }
+        )
+      );
+    }
   }
 
   const diceElements = dice.map(({ value, isHeld, id }) => (
-    <Die key={id} value={value} isHeld={isHeld} />
+    <Die key={id} id={id} value={value} isHeld={isHeld} hold={hold} />
   ));
 
   return (
     <main>
+      {gameWon && (
+        <Confetti width={window.innerWidth} height={window.innerHeight} />
+      )}
+      <div aria-live="polite" className="sr-only">
+        {gameWon && (
+          <p>Congratulations! You won! Press "New Game" to start again.</p>
+        )}
+      </div>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       <div className="dice-container">{diceElements}</div>
-      <button onClick={rollDice}>Roll</button>
+      <button ref={newGameBtn} className="roll-dice" onClick={rollDice}>
+        {gameWon ? "New Game" : "Roll"}
+      </button>
     </main>
   );
 }
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,4 +1,15 @@
 export default function Die(props) {
   const buttonClass = props.isHeld ? "die selected" : "die";
-  return <button className={buttonClass}>{props.value}</button>;
+  return (
+    <button
+      onClick={() => props.hold(props.id)}
+      className={buttonClass}
+      aria-pressed={props.isHeld}
+      aria-lable={`Die with value of ${props.value} is ${
+        props.isHeld ? "held" : "not held"
+      }`}
+    >
+      {props.value}
+    </button>
+  );
 }
